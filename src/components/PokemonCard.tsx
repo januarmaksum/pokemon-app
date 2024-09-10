6cd8909dc9c3b871ec06cd153d24e06341d7c8a4
Januar Maksum <januarmaksum@gmail.com>
2024-09-10 20:11:40 +0700

    wip: add detail

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,25 +1,36 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 interface PokemonCardProps {
-    name: string;
-    id?: number;
-    imageUrl: string;
-  }
-  
-  export default function PokemonCard({ name, imageUrl }: PokemonCardProps) {
-    return (
-      <div className="bg-white dark:bg-gray-800 rounded-lg shadow-md overflow-hidden">
-        <Image
-          src={imageUrl}
-          alt={name}
-          className="w-full h-32 object-contain"
-          width={230}
-          height={230}
-        />
-        <div className="p-4 text-center">
-          <h2 className="text-xl font-semibold capitalize text-gray-800 dark:text-white">{name}</h2>
-        </div>
+  name: string;
+  id?: number;
+  imageUrl: string;
+}
+
+export default function PokemonCard({ name, imageUrl }: PokemonCardProps) {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push(`/pokemon/${name}`);
+  };
+
+  return (
+    <div
+      onClick={handleClick}
+      className="bg-white dark:bg-gray-800 rounded-lg shadow-md overflow-hidden cursor-pointer"
+    >
+      <Image
+        src={imageUrl}
+        alt={name}
+        className="w-full h-32 object-contain"
+        width={230}
+        height={230}
+      />
+      <div className="p-4 text-center">
+        <h2 className="text-xl font-semibold capitalize text-gray-800 dark:text-white">
+          {name}
+        </h2>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
--- src/pages/pokemon/[slug].tsx
+++ src/pages/pokemon/[slug].tsx
@@ -1,8 +1,71 @@
-import { useRouter } from "next/router";
+import { GetServerSideProps } from "next";
+import axios from "axios";
 import Layout from "@/components/layout";
+import { ParsedUrlQuery } from "querystring";
+import Image from "next/image";
 
-export default function Detail() {
-  const router = useRouter();
-  const title = String(router.query.slug || "");
-  return <Layout title={title}>Detail {router.query.slug}</Layout>;
+interface PokemonDetailPageProps {
+  pokemon: {
+    name: string;
+    id: number;
+    imageUrl: string;
+    height: number;
+    weight: number;
+  };
 }
+
+export default function PokemonDetailPage({ pokemon }: PokemonDetailPageProps) {
+  return (
+    <Layout title={pokemon.name}>
+      <div className="container mx-auto px-4">
+        <div className="bg-white dark:bg-gray-800 rounded-lg shadow-md p-6">
+          <Image
+            src={pokemon.imageUrl}
+            alt={pokemon.name}
+            className="w-full h-64 object-contain"
+            width={230}
+            height={230}
+          />
+          <h1 className="text-4xl font-bold capitalize">{pokemon.name}</h1>
+          <p>ID: {pokemon.id}</p>
+          <p>Height: {pokemon.height} decimetres</p>
+          <p>Weight: {pokemon.weight} hectograms</p>
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+interface Params extends ParsedUrlQuery {
+  slug: string;
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { slug } = context.params as Params;
+
+  try {
+    const response = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon/${slug}`
+    );
+    const { id, name, height, weight, sprites } = response.data;
+    const imageUrl =
+      sprites.other.dream_world.front_default || sprites.front_default;
+
+    return {
+      props: {
+        pokemon: {
+          name,
+          id,
+          height,
+          weight,
+          imageUrl,
+        },
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching Pokémon data:", error);
+    return {
+      notFound: true,
+    };
+  }
+};
