d6986fe0f260f0a7e5c2cb96d5f7b9c504c5a688
Januar Maksum <januarmaksum@gmail.com>
2024-09-11 12:25:14 +0700

    add attr lang

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,14 +7,22 @@ import { Toaster } from "react-hot-toast";
 interface LayoutProps {
   children: React.ReactNode;
   title: string;
+  lang?: string;
 }
 
-const Layout = ({ children, title = "" }: LayoutProps) => {
+const Layout = ({ children, title = "", lang = "en" }: LayoutProps) => {
+  React.useEffect(() => {
+    document.documentElement.lang = lang;
+  }, [lang]);
+
   return (
     <>
       <Head>
         <title>{title}</title>
-        <meta name="description" content="View a list of Pokémon, see details about each Pokémon, and catch Pokémon to store in their own collection." />
+        <meta
+          name="description"
+          content="View a list of Pokémon, see details about each Pokémon, and catch Pokémon to store in their own collection."
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
       </Head>
